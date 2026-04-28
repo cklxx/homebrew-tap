662fe7c2ee9f4fc128cbc22f3b364e6899de022c
cklxx <1293822641@qq.com>
2026-04-28 13:26:10 +0800

    arle v0.1.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/arle.rb
+++ Formula/arle.rb
@@ -1,9 +1,9 @@
 class Arle < Formula
   desc "Pure-Rust runtime for serving, local agents, training, and evaluation"
   homepage "https://github.com/cklxx/arle"
-  url "https://github.com/cklxx/arle/releases/download/v0.1.3/arle-v0.1.3-macos-arm64.tar.gz"
-  version "0.1.3"
-  sha256 "c77867e6b88d16b28da5ef0e466f52cf04d195f4d4942b4b0b4b4b37aeb0025c"
+  url "https://github.com/cklxx/arle/releases/download/v0.1.4/arle-v0.1.4-macos-arm64.tar.gz"
+  version "0.1.4"
+  sha256 "c72cf517ec2c1757e4e928a459b6738240b47516c29840538fa63135eb1201d1"
   license "MIT"
 
   # macOS arm64 only. Linux users: see
